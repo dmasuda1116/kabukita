fe516b39089511c5a70b35e5b820e0763f785ec4
dmasuda1116 <dmasuda1116@gmail.com>
2023-12-23 16:24:52 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import { SelectChangeEvent } from '@mui/material/Select';
 interface FormState {
   name: string;
   gender: string;
-  affiliation: string;
-  clubName: string;
-  source: string;
-  phoneNumber: string;
-  breakfastPreference: string;
+  age: string;
+  businessActivity: string;
+  businessScale: string;
+  numberOfEmployees: string;
+  businessStartDate: string;
 }
 
 const App = () => {
@@ -35,60 +35,45 @@ const App = () => {
   const [formState, setFormState] = useState<FormState>({
     name: '',
     gender: '',
-    affiliation: '',
-    clubName: '',
-    source: '',
-    phoneNumber: '',
-    breakfastPreference: ''
+    age: '',
+    businessActivity: '',
+    businessScale: '',
+    numberOfEmployees: '',
+    businessStartDate: ''
   });
 
-// This will be used for the TextField components
-const handleTextInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-  const name = event.target.name;
-  const value = event.target.value;
+  const handleTextInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
 
-  if (typeof name === "string" && typeof value === "string") {
-    setFormState({
-      ...formState,
-      [name]: value
-    });
-  }
-};
-
-// This will be used for the Select components
-const handleSelectInputChange = (event: SelectChangeEvent<string>) => {
-  const name = event.target.name;
-  const value = event.target.value;
-
-  if (typeof name === "string" && typeof value === "string") {
-    setFormState({
-      ...formState,
-      [name]: value
-    });
-  }
-};
-  
+    if (typeof name === "string" && typeof value === "string") {
+      setFormState({
+        ...formState,
+        [name]: value
+      });
+    }
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setErrors([]);  // Clear out previous errors
+    setErrors([]); // Clear out previous errors
 
     // Input validation
     let tempErrors: string[] = [];
-    for (let key in formState) {
-      if (formState[key as keyof FormState] === '') {
-        tempErrors.push(`${key} を入力してください。`);
-      }
+    if (formState.name === '') {
+      tempErrors.push(`氏名を入力してください。`);
     }
+
+    // Add additional validation as needed
 
     setErrors(tempErrors);
 
     // Send the form data to LINE bot via LIFF
-    if(tempErrors.length === 0 && liff.isLoggedIn()) {
+    if (tempErrors.length === 0 && liff.isLoggedIn()) {
       try {
         await liff.sendMessages([{
           type: 'text',
-          text: JSON.stringify(formState)
+          text: "フォーム初回登録\n" + JSON.stringify(formState)
         }]);
         liff.closeWindow();
       } catch (err) {
@@ -101,63 +86,36 @@ const handleSelectInputChange = (event: SelectChangeEvent<string>) => {
     <Container>
       <form onSubmit={handleSubmit}>
         <Grid container spacing={3}>
+          {/* 各フィールドの追加 */}
           <Grid item xs={12}>
-            <Typography variant="h6" component="div">名前</Typography>
+            <Typography variant="h6" component="div">氏名</Typography>
             <TextField name="name" onChange={handleTextInputChange} required fullWidth />
           </Grid>
           <Grid item xs={12}>
-          <Typography variant="h6" component="div">性別</Typography>
-            <FormControl fullWidth>
-            <Select name="gender" value={formState.gender} onChange={handleSelectInputChange} required>
-                <MenuItem value="">--選択してください--</MenuItem>
-                <MenuItem value="Male">男性</MenuItem>
-                <MenuItem value="Female">女性</MenuItem>
-                <MenuItem value="Other">その他</MenuItem>
-              </Select>
-            </FormControl>
+            <Typography variant="h6" component="div">性別</Typography>
+            <TextField name="gender" onChange={handleTextInputChange} required fullWidth />
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h6" component="div">所属</Typography>
-            <FormControl fullWidth>
-              <Select name="affiliation" onChange={handleSelectInputChange} required>
-                <MenuItem value="">--選択してください--</MenuItem>
-                <MenuItem value="自単会">自単会</MenuItem>
-                <MenuItem value="他単会">他単会</MenuItem>
-                <MenuItem value="ゲスト">ゲスト</MenuItem>
-              </Select>
-            </FormControl>
+            <Typography variant="h6" component="div">年齢</Typography>
+            <TextField name="age" onChange={handleTextInputChange} required fullWidth />
           </Grid>
-  
-          {formState.affiliation === "他単会" && (
-            <Grid item xs={12}>
-              <Typography variant="h6" component="div">単会名</Typography>
-              <TextField name="clubName" onChange={handleTextInputChange} fullWidth />
-            </Grid>
-          )}
-  
-          {formState.affiliation === "ゲスト" && (
-            <>
-              <Grid item xs={12}>
-                <Typography variant="h6" component="div">どのように知りましたか?</Typography>
-                <TextField name="source" onChange={handleTextInputChange} fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="h6" component="div">電話番号</Typography>
-                <TextField name="phoneNumber" onChange={handleTextInputChange} fullWidth />
-              </Grid>
-            </>
-          )}
-  
           <Grid item xs={12}>
-            <Typography variant="h6" component="div">朝食は必要ですか?</Typography>
-            <FormControl component="fieldset" required>
-              <RadioGroup aria-label="breakfastPreference" name="breakfastPreference" value={formState.breakfastPreference} onChange={handleTextInputChange}>
-                <FormControlLabel value="Yes" control={<Radio />} label="はい" />
-                <FormControlLabel value="No" control={<Radio />} label="いいえ" />
-              </RadioGroup>
-            </FormControl>
+            <Typography variant="h6" component="div">事業内容</Typography>
+            <TextField name="businessActivity" onChange={handleTextInputChange} required fullWidth />
           </Grid>
-  
+          <Grid item xs={12}>
+            <Typography variant="h6" component="div">事業規模 (年商/利益)</Typography>
+            <TextField name="businessScale" onChange={handleTextInputChange} required fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h6" component="div">従業員数</Typography>
+            <TextField name="numberOfEmployees" onChange={handleTextInputChange} required fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h6" component="div">事業開始時期</Typography>
+            <TextField name="businessStartDate" onChange={handleTextInputChange} required fullWidth />
+          </Grid>
+
           <Grid item xs={12}>
             <Button type="submit" variant="contained" color="primary" fullWidth>送信</Button>
           </Grid>
@@ -174,4 +132,3 @@ const handleSelectInputChange = (event: SelectChangeEvent<string>) => {
 };
 
 export default App;
-
